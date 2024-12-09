d7439120e39554c9c8c49bd1e271c6afd5a91685
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-12-09 11:01:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:3000`);
 });
