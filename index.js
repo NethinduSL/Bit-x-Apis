b724db811c5eb9b97b9fc71f37419155d0416319
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-12-10 08:01:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,22 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express(); // Initialize the Express app
-const apiFolderPath = path.join(__dirname, 'API'); // Path to API folder
 
-// Dynamically load and use all route files in the folder
-fs.readdirSync(apiFolderPath).forEach(file => {
-    const filePath = path.join(apiFolderPath, file);
-
-    // Ensure the file is a JavaScript file
-    if (file.endsWith('.js')) {
-        const routes = require(filePath);
-
-        // Check if the file exports a function to attach to the app
-        if (typeof routes === 'function') {
-            routes(app); // Pass the app instance to the route
-        }
-    }
-});
 
 // Root route
 app.get('/', (req, res) => {
