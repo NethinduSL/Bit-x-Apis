775b14b89f8383e78e5d8b90ef52dc68380d0d04
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-12-10 08:14:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,27 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express(); // Initialize the Express app
+const apiFolderPath = path.join(__dirname, 'Bitx'); // Path to API folder
 
+// Dynamically load and use all route files in the folder
+fs.readdirSync(apiFolderPath).forEach(file => {
+    const filePath = path.join(apiFolderPath, file);
+
+    // Ensure the file is a JavaScript file
+    if (file.endsWith('.js')) {
+        const routes = require(filePath);
+
+        // Check if the file exports a function to attach to the app
+        if (typeof routes === 'function') {
+            routes(app); // Pass the app instance to the route
+        }
+    }
+});
 
 // Root route
 app.get('/', (req, res) => {
     res.send('Bit x API server is running!');
 });
-
-
-app.get('/details', (req, res) => {
-    res.json({ message: 'Details from About API' });
-});
-
-
 
 // Export the app for Vercel
 module.exports = app;
