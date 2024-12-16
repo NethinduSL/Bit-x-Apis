562fca57150b16117a1f67dcedab86f0ba9116d2
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2024-12-16 21:14:12 +0530

    Update news.js

--- BitX/news.js
+++ BitX/news.js
@@ -1,38 +1,32 @@
+const fs = require('fs');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-async function hiru(startId = 390861) {
-  let currentId = startId;
-  let latestArticle = null;
+const hiru = async () => {
+    let currentId = 390689;
 
-  try {
     while (true) {
-      try {
-        const url = `https://www.hirunews.lk/${currentId}`;
-        const response = await axios.get(url);
+        try {
+            const url = `https://www.hirunews.lk/${currentId}`;
+            const response = await axios.get(url);
+            const $ = cheerio.load(response.data);
 
-        const $ = cheerio.load(response.data);
-        const title = $('h1.main-tittle').text().trim();
-        const image = $('img.lazyload').attr('data-src');
-        const textContainer = $('#article-phara2');
-        textContainer.find('iframe').remove();
-        const text = textContainer.html()?.replace(/<br\s*\/?>/gi, '') || '';
+            const title = $('h1.main-tittle').text().trim();
+            const image = $('img.lazyload').attr('data-src');
+            const textContainer = $('#article-phara2');
+            textContainer.find('iframe').remove();
+            const text = textContainer.html().replace(/<br\s*\/?>/gi, '');
 
-        if (title && image && text) {
-          latestArticle = { id: currentId, title, image, text };
-          currentId++;
-        } else {
-          currentId++; // Skip invalid article and move to next ID
+            return {
+                id: currentId,
+                title,
+                image,
+                text
+            };
+        } catch (error) {
+            currentId++;
         }
-      } catch (error) {
-        currentId++; // Skip to next ID on error
-      }
     }
-  } catch (error) {
-    return {
-      message: `Failed to fetch the news: ${error.message || 'Unknown error'}`,
-    };
-  }
 }
 
 module.exports = { hiru };
