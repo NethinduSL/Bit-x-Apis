e69415b6dc7d9e7d85593c62f58d71c0d0c50462
NethinduSL <144025781+NethinduSL@users.noreply.github.com>
2025-08-29 17:46:34 +0530

    Update dta.js

--- BitX/dta.js
+++ BitX/dta.js
@@ -1,1 +1,18 @@
 
+// dta.js
+const data = {
+  start: {
+    name: "hi",
+    link: "link",
+    times: 10,
+    price: "40RS"
+  },
+  end: {
+    name: "ended",
+    link: "link",
+    times: 0, // view count ended
+    price: "40RS"
+  }
+};
+
+module.exports = data;
